8a55bbec72fc81d4e3e48fa3abad3b81c0784ea3
Robin Cloutman <rycochet@rycochet.com>
2011-09-21 06:55:15 +0100

    First commit. Blank project.

---
+++ src/main/java/mmo/Quest/MMOQuest.java
@@ -0,0 +1,41 @@
+/*
+ * This file is part of mmoMinecraft (https://github.com/mmoMinecraftDev).
+ *
+ * mmoMinecraft is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package mmo.Quest;
+
+import java.util.BitSet;
+import mmo.Core.MMOPlugin;
+
+public class MMOQuest extends MMOPlugin {
+
+	@Override
+	public BitSet mmoSupport(BitSet support) {
+		support.set(MMO_DATABASE);
+		return support;
+	}
+
+	@Override
+	public void onEnable() {
+		super.onEnable();
+
+	}
+
+	@Override
+	public void onDisable() {
+		super.onDisable();
+
+	}
+}

85d6167dc88279e620ed4cd38b263b9b3d13cc3d
Wulfspider <hello@wulfspider.com>
2012-05-16 11:40:09 -0400

    Updated pom.xml, readme, plugin.yml, and added header.txt. Added automatic license header checking and formatting. Formatted headers to be project specific. A little bit more formatting on misc files.

--- src/main/java/mmo/Quest/MMOQuest.java
+++ src/main/java/mmo/Quest/MMOQuest.java
@@ -1,7 +1,7 @@
 /*
- * This file is part of mmoMinecraft (https://github.com/mmoMinecraftDev).
+ * This file is part of mmoQuest <http://github.com/mmoMinecraftDev/mmoQuest>.
  *
- * mmoMinecraft is free software: you can redistribute it and/or modify
+ * mmoQuest is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
